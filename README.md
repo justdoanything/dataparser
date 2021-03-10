2abad556bc54e1e0f1278268a4a791817ec817c5
82736 <82736@LCSC08V899.corp.lgcns.com>
2021-03-10 12:45:23 +0900

    Use Sample

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dataparser
+
+We need to parse a type of data if we do a data migration or move the data from file to database sometimes.
+
+This code makes it easy for us to convert the data.
+  1. JSON format ¡æ Excel Format
+  2. Excel format ¡æ Insert Query
+  3. Interface data with JSON format ¡æ Excel format
+  
+You can copy the chart of excel and then copy to text file if you want to use excel data.
+
+  
--- src/Runner/MainClass.java
+++ src/Runner/MainClass.java
@@ -14,26 +14,23 @@ public class MainClass {
 		JSONToExcel jte = new JSONToExcel();
 		CheckData cd = new CheckData();
 		
-		String filePath = "C:\\Users\\82736\\Desktop\\test.txt";
+		String filePath = "D:\\01.OBS\\21.ÀÌ¿ë¿ì\\40.Interface\\30 Rollout\\21 GP2\\SA\\product.txt";
 		String tableName = "";
 		boolean autoFileOpen = false;
 
-//		tableName = MsgCode.Product;
-//		tableName = MsgCode.Price;
-		tableName = MsgCode.Order_Response;
-//		tableName = MsgCode.Stock; 
+		tableName = MsgCode.sample;
 
 		//webmethod ¡æ Excel ¡æ Insert Query
-		String result = wte.makeExcelData(filePath, autoFileOpen);	etiq.makeInsertQuery(result, tableName, autoFileOpen);
+//		String result = wte.makeExcelData(filePath, autoFileOpen);	etiq.makeInsertQuery(result, tableName, autoFileOpen);
 		
 		//JSON ¡æ Excel Form
 //		jte.makeExcelData(filePath, autoFileOpen);
 		
-		//webmethod ¡æ Excel Form
+		//webmethod ¡æ Excel Formvmfwpr
 //		result = wte.makeExcelData(filePath, autoFileOpen);
 		
 		//Excel ¡æ Insert Query
-//		etiq.makeInsertQuery(filePath, tableName, autoFileOpen);
+		etiq.makeInsertQuery(filePath, tableName, autoFileOpen);
 		
 //		cd.checking(filePath, autoFileOpen);
 	}
